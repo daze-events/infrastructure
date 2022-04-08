d824c8633d416273795e557ef1caa2bdf90aa22d
Alexander James Candfield <alex.candfield@gmail.com>
2022-04-08 20:37:55 +0200

    Removed backend from VPC module

--- modules/networking/vpc-app/main.tf
+++ modules/networking/vpc-app/main.tf
@@ -1,8 +1,4 @@
 terraform {
-  # The configuration for this backend will be filled in by Terragrunt or via a backend.hcl file. See
-  # https://www.terraform.io/docs/backends/config.html#partial-configuration
-  backend "s3" {}
-
   # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
   # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
   required_version = "= 1.1.8"
