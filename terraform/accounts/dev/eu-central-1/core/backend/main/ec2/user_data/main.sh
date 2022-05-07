3f9bd8ec1dce4a8fe1b7d1a7e9999d91b67d191e
Alexander James Candfield <40602303+AJCandfield@users.noreply.github.com>
2022-05-08 01:29:21 +0200

    Packer + Ansible improvements (#23)
    * Improving Packer pipeline (#20)
    
    * Corrected branch array
    
    * Update README.md
    
    * Added work dir to packer pipeline step
    
    * Added init to packer script
    
    * Corrected init folder
    
    * simplifying ssh keys
    
    * simplifying ssh keys
    
    * added dev branch to pipeline trigger
    
    * Packer: pipeline (#21)
    
    * Corrected branch array
    
    * Update README.md
    
    * Added work dir to packer pipeline step
    
    * Added init to packer script
    
    * Corrected init folder
    
    * simplifying ssh keys
    
    * simplifying ssh keys
    
    * added dev branch to pipeline trigger
    
    * Extended auto_deploy permissions
    
    * Packer: pipeline (#22)
    
    * Corrected branch array
    
    * Update README.md
    
    * Added work dir to packer pipeline step
    
    * Added init to packer script
    
    * Corrected init folder
    
    * simplifying ssh keys
    
    * simplifying ssh keys
    
    * added dev branch to pipeline trigger
    
    * Extended auto_deploy permissions
    
    * Extended auto_deploy permissions

--- terraform/accounts/dev/eu-central-1/core/backend/main/ec2/user_data/main.sh
+++ terraform/accounts/dev/eu-central-1/core/backend/main/ec2/user_data/main.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 {
-    AWS_REGION="$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')"
+    AWS_REGION="$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | grep region | awk -F\" '{print $4}')"
     AWS_SHARED_ACCOUNT_ID="169567906943"
     AWS_REGISTRY_ADDRESS="${AWS_SHARED_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
     AWS_REPO_NAME="infrastructure/ansible"
@@ -10,6 +10,13 @@ set -eo pipefail
 
     aws ecr get-login-password --region "${AWS_REGION}" | docker login --username AWS --password-stdin "${AWS_REGISTRY_ADDRESS}"
 
-    docker run --rm "${AWS_REGISTRY_ADDRESS}/${AWS_REPO_NAME}:${AWS_IMAGE_TAG}"
+    ssh-keygen -t rsa -b 4096 -C "daze-events" -f /tmp/daze-events -N ""
+
+    cat /tmp/daze-events.pub > /home/ansible/.ssh/authorized_keys
+
+    docker run --rm -v /tmp/daze-events:/etc/ansible/ssh.pem "${AWS_REGISTRY_ADDRESS}/${AWS_REPO_NAME}:${AWS_IMAGE_TAG}"
+
+    rm -v /tmp/daze-events*
+    > /home/ansible/.ssh/authorized_keys
 
 } &>>/var/log/user-data.log
