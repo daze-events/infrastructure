35f9a14e14839d3754e72441902269284cbba6d9
Alexander James Candfield <alex.candfield@gmail.com>
2022-05-07 16:08:54 +0200

    module: added registry policies to pull images

--- terraform/modules/ec2/single-server/data.tf
+++ terraform/modules/ec2/single-server/data.tf
@@ -3,6 +3,17 @@ data "aws_iam_policy_document" "main" {
     effect = "Allow"
     actions = [
       "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:GetRepositoryPolicy",
+      "ecr:DescribeRepositories",
+      "ecr:ListImages",
+      "ecr:DescribeImages",
+      "ecr:BatchGetImage",
+      "ecr:GetLifecyclePolicy",
+      "ecr:GetLifecyclePolicyPreview",
+      "ecr:ListTagsForResource",
+      "ecr:DescribeImageScanFindings",
     ]
     resources = ["*"]
   }
